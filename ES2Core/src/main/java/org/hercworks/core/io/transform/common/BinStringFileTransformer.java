6f149f724e6f3892ae8edfc587675564e43d3fae
subject9x <pwroohr@gmail.com>
2025-06-10 22:38:40 -0400

    update: String .BIN files and transformers are good to go, thanks to Crow! for mapping this one.

--- ES2Core/src/main/java/org/hercworks/core/io/transform/common/BinStringFileTransformer.java
+++ ES2Core/src/main/java/org/hercworks/core/io/transform/common/BinStringFileTransformer.java
@@ -1,10 +1,12 @@
 package org.hercworks.core.io.transform.common;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import org.hercworks.core.data.file.StringBinaryFile;
 import org.hercworks.core.io.transform.ThreeSpaceByteTransformer;
 import org.hercworks.voln.DataFile;
+import org.hercworks.voln.FileType;
 
 public class BinStringFileTransformer extends ThreeSpaceByteTransformer {
 
@@ -14,31 +16,77 @@ public class BinStringFileTransformer extends ThreeSpaceByteTransformer {
 		if(inputArray == null) {
 			return null;
 		}
+		index = 0;
 		setBytes(inputArray);
 		
-		StringBinaryFile bin = new StringBinaryFile();
-		bin.setRawBytes(inputArray);
+		StringBinaryFile binFile = new StringBinaryFile();
+		binFile.setRawBytes(inputArray);
+		binFile.setExt(FileType.BIN);
 		
-		bin.setTotalStrings(indexIntLE());
+		//note - the reading here ditches the structure of the file,
+		// writing out to the format will do the metadata generation.
+		int totalStrings = indexIntLE();
 		
-		skip(4);	//UINT32 of total bytes.
+		skip(4);	//skips total strings size, this is not needed.
 		
-		for(int i = 0; i < bin.getTotalStrings(); i++) {
+		int indexStart = index;
+		int stringStart = index + (totalStrings * 2);
+		
+		String[] values = new String[totalStrings];
+		for(int i=0; i < totalStrings; i++) {
+			index = indexStart + (i * 2);
+			int offset = indexShortLE();
 			
+			if(i < totalStrings - 1) {
+				index = indexStart + ((i+1) * 2);
+				int nextIndex = indexShortLE(); 
+				
+				index = stringStart + offset;
+				values[i] = indexString(nextIndex - offset).trim();
+			}
+			else {
+				index = stringStart + offset;
+				values[i] = indexString(inputArray.length - index).trim();
+			}
 		}
 		
+		binFile.setValues(values);
 		
-		
-		return bin;
+		return binFile;
 	}
 
 	@Override
 	public byte[] objectToBytes(DataFile source) throws ClassCastException, IOException {
 		
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		
+		if(source == null) {
+			return null;
+		}
+
+		StringBinaryFile sbf = (StringBinaryFile)source;
 		
 		
+		short[] index = new short[sbf.getValues().length];
 		
-		return null;
+		int size = 0;
+		for(int s=0; s < sbf.getValues().length; s++) {
+			index[s] = (short)size;
+			size += sbf.getValues()[s].length();
+			size += sbf.getValues()[s].endsWith(" ") ? 0 : 1; //null terminal byte
+		}
+		
+		out.write(writeIntLE(sbf.getValues().length));
+		out.write(writeIntLE(size));
+		for(int i=0; i < sbf.getValues().length; i++) {
+			out.write(writeShortLE(index[i]));
+		}
+		for(int t=0; t < sbf.getValues().length; t++) {
+			out.write(sbf.getValues()[t].getBytes());
+			out.write(0x00);
+		}
+		
+		return out.toByteArray();
 	}
 
 }
--- ES2Core/src/test/java/org/hercworks/core/api/TestTransformers.java
+++ ES2Core/src/test/java/org/hercworks/core/api/TestTransformers.java
@@ -1,12 +1,52 @@
 package org.hercworks.core.api;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.hercworks.core.data.file.StringBinaryFile;
+import org.hercworks.core.io.transform.common.BinStringFileTransformer;
 import org.junit.Test;
 
 public class TestTransformers {
 
 	@Test
-	public void testPDG() {
+	public void testBIN() {
 		
+		BinStringFileTransformer binTrs = new BinStringFileTransformer();
+		File file = new File("e:/es2_os/dev/earthsiege2/unpack/lang0/eng/ESNAMES.bin");
+		
+		StringBinaryFile strbin = null;
+		try {
+			FileInputStream fizz = new FileInputStream(file);
+			
+			byte[] b = fizz.readAllBytes();
+			fizz.close();
+			
+			strbin = (StringBinaryFile)binTrs.bytesToObject(b);
+			
+			
+			File es = new File("e:/es2_os/dev/earthsiege2/eng/ESNAMES.bin");
+			
+			byte[] out = binTrs.objectToBytes(strbin);
+			
+			FileOutputStream fout = new FileOutputStream(es);
+			fout.write(out);
+			fout.close();
+			
+			
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace(System.out);
+		} 
+		catch (IOException e) {
+			e.printStackTrace(System.out);
+		}
+		catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
 		
 	}
 	
